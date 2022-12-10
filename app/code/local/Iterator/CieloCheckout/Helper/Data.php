ff53a117e77dfcd60ebabb074bbe94fa9c72a4e0
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2022-12-09 21:56:01 -0300

    Commit referente ao issue #1
    Estruturação do Módulo

---
+++ app/code/local/Iterator/CieloCheckout/Helper/Data.php
@@ -0,0 +1,33 @@
+<?php
+ /**
+ * Iterator Sistemas Web
+ *
+ * NOTAS SOBRE LICENÇA
+ *
+ * Este arquivo de código-fonte está em vigência dentro dos termos da EULA.
+ * Ao fazer uso deste arquivo em seu produto, automaticamente você está 
+ * concordando com os termos do Contrato de Licença de Usuário Final(EULA)
+ * propostos pela empresa Iterator Sistemas Web.
+ *
+ * =================================================================
+ *               MÓDULO DE PAGAMENTOS CIELO CHECKOUT
+ * =================================================================
+ * Este produto foi desenvolvido para o Ecommerce Magento de forma a
+ * possibilitar a implementação de recursos para pagamentos por meio
+ * de integração com o Checkout Cielo.
+ * Através deste módulo a loja virtual do contratante do serviço
+ * passará redirecionará o cliente para que finalize o pagamento
+ * em ambiente seguro da Cielo.
+ * =================================================================
+ *
+ * @category   Iterator
+ * @package    Iterator_CieloCheckout
+ * @author     Ricardo Auler Barrientos <contato@iterator.com.br>
+ * @copyright  Copyright (c) Iterator Sistemas Web - CNPJ: 19.717.703/0001-63
+ * @license    O Produto é protegido por leis de direitos autorais, bem como outras leis de propriedade intelectual.
+ */
+
+class Iterator_CieloCheckout_Helper_Data extends Mage_Core_Helper_Abstract {
+}
+
+?>
